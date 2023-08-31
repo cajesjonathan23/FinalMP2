11882ca569f8259b03624af94bf56285a5d6026a
cajesjonathan23 <jonathan17cajes@gmail.com>
2023-08-31 10:30:24 +0800

    Update

--- index.js
+++ index.js
@@ -72,4 +72,19 @@ cartLogo.addEventListener("click", function (e) {
     cartIcon.href = "./cart.html"
   })
 
-  
+  //scroll to top
+  // JavaScript to show/hide the scroll to top button
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll-to-top').fadeIn();
+        } else {
+            $('#scroll-to-top').fadeOut();
+        }
+    });
+
+    $('#scroll-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+});
